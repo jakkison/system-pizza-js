5281f833fc2ed97f4f9a4b2ba6691fb08a575631
Jacson C dos Santos <jacson@testingcompany.com.br>
2020-09-18 10:22:45 -0300

    Mostrar/esconder itens no carrinho

---
+++ js/pizzas.js
@@ -0,0 +1,66 @@
+//PRODUTO: Itens de pizzas para serem utilizados no sistema
+
+let pizzaJson = [
+    { 
+     id: 1, 
+     name: 'Mussarela',
+     img: 'img/pizza.png',
+     price: 20.19,
+     sizes: ['100g', '530g', '860g'], 
+     desc: 'Pizza de Mussarela, uma delicia de sabor.'
+    },
+
+    { 
+        id: 2, 
+        name: 'Calabresa', 
+        img: 'img/pizza2.png', 
+        price: 18.00, 
+        sizes: ['320g', '530g', '860g'], 
+        desc: 'Pizza de Calabresa, uma delicia de sabor.' 
+    },
+    
+    { 
+        id: 3, 
+        name: 'Quatro Queijos', 
+        img: 'img/pizza3.png', 
+        price: 17.45, 
+        sizes: ['320g', '530g', '860g'], 
+        desc: 'Pizza de Quatro Queijos, uma delicia de sabor.' 
+    },
+
+    {
+        id: 4, 
+        name: 'Americana', 
+        img: 'img/pizza4.png', 
+        price: 19.77, 
+        sizes: ['320g', '530g', '860g'], 
+        desc: 'Pizza de Sabor Americano, uma delicia de pizza.' 
+    },
+    
+    { 
+        id: 5, 
+        name: 'Sorvete', 
+        img: 'img/pizza5.png', 
+        price: 21.43, 
+        sizes: ['320g', '530g', '860g'], 
+        desc: 'Pizza de Sorvete, uma delicia de sabor.'
+     },
+    
+     { 
+         id: 6, 
+         name: 'Moda da Casa', 
+         img: 'img/pizza6.png', 
+         price: 18.55, 
+         sizes: ['320g', '530g', '860g'], 
+         desc: 'Pizza de Moda Da Casa, uma delicia de sabor.'
+     },
+    
+     { 
+         id: 7, 
+         name: 'Chocolate', 
+         img: 'img/pizza7.png', 
+         price: 22.36, 
+         sizes: ['320g', '530g', '860g'], 
+         desc: 'Pizza de Chocolate, uma delicia de sabor, que derrete na boca.'
+     }
+];
---
+++ js/script.js
@@ -0,0 +1,119 @@
+let cart = [];
+let modalQt = 1;
+let modalKey = 0;
+
+/*(dqs)=> mesmo que function*/
+const qs = (dqs) => document.querySelector(dqs);
+const qss = (dqs) => document.querySelectorAll(dqs);
+
+//Listagem das Pizzas
+pizzaJson.map(function(item, index) {
+    //clonar uma estrutura html
+    let pizzaItem = qs('.models .pizza-item').cloneNode(true); // pega cada parte redondinha das pizas e clona
+    
+    pizzaItem.setAttribute('data-key', index); // insere em cada parte redondinha os dados de seu index
+    // inserir informações na pizza 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.desc;
+    
+    // clica no item e abre modal
+    pizzaItem.querySelector('a').addEventListener('click', (evento)=>{
+        evento.preventDefault();// nao REcarrega a pagina ao clicar em link
+        let key = evento.target.closest('.pizza-item').getAttribute('data-key'); // procura o elemento mais proximo pizza-item
+        modalQt = 1;
+        modalKey = key;
+
+        // preencher modal com as informações
+        qs('.pizzaBig img').src = pizzaJson[key].img;
+        qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].desc;
+        qs('.pizzaInfo--actualPrice').innerHTML =  `R$ ${pizzaJson[key].price.toFixed(2)}`;// formatar campo Dinheiro
+       
+       qs('.pizzaInfo--size.selected').classList.remove('selected');
+       qss('.pizzaInfo--size').forEach((size, sizeIndex)=>{    
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+        qs(".pizzaInfo--qt").innerHTML = modalQt;
+
+        //Abre modal com animação de opacidade css + js
+        qs('.pizzaWindowArea').style.opacity = 0;
+        qs('.pizzaWindowArea').style.display = 'flex';// abre a modal do item
+        setTimeout(()=> {
+            qs('.pizzaWindowArea').style.opacity = 1;
+        },200);
+    });
+
+    //Pega um elemento html e joga na tela
+    qs('.pizza-area').append(pizzaItem); // adicionar mais itens onde ja existe
+});
+
+// Eventos do Modal
+
+function closeModal(){
+    qs('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() =>{
+        qs('.pizzaWindowArea').style.display = 'none';
+    },500);
+};
+
+// faz botões cancelar e voltar chamarem a função closeModal();
+qss(".pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton").forEach((item) => {
+    item.addEventListener("click", closeModal);
+});
+
+// adicionar evento nos botões de quantidade
+qs('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt >= 1){
+        modalQt--;
+        qs(".pizzaInfo--qt").innerHTML = modalQt;
+    }
+});
+
+qs(".pizzaInfo--qtmais").addEventListener('click', () => {
+    modalQt++;
+    qs(".pizzaInfo--qt").innerHTML = modalQt;
+});
+
+// Adicionar funcionalidade nos botões de quantidade
+qss('.pizzaInfo--size').forEach((size, sizeIndex)=>{    
+    size.addEventListener('click', (evento)=>{
+        qs('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+
+// Adicionar Itens selecionado ao carrinho
+qs('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(qs(".pizzaInfo--size.selected").getAttribute("data-key"));
+    let id = `${pizzaJson[modalKey].id}@${size}`;
+    let key = cart.findIndex((item) => item.idCode == id);
+    if (key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            idCode: id,
+            id: pizzaJson[modalKey].id,
+            size,
+            qt: modalQt
+        })
+    }
+    closeModal();
+    updateCart();
+});
+
+//atualizar carrinho
+function updateCart(){
+    if (cart.length > 0) {// caso tenha itens no carrinho ele é mostrado
+        qs('aside').classList.add('show');
+    }else{ // se não tiver itens o carrinho não fica na tela
+        qs('aside').classList.remove('show');
+    }
+}
+
+
+
